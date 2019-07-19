a49b2d3119793ff25253e2c542aa73eb025a8432
Amal Majeed <amalmajeed7@gmail.com>
2019-07-19 20:28:23 +0530

    DB Config changed to fit mongoLabs

--- common/database.py
+++ common/database.py
@@ -1,11 +1,10 @@
 from typing import Dict
 import pymongo
-import os
+#import os
 
 class Database(object):
-    URI = os.environ.get('MONGOLAB_URI')
-    Client = pymongo.MongoClient(URI)
-    DATABASE = Client['heroku_jnzbgb2c']
+    URI = "mongodb://amalmajeed:Godblessus786@ds151817.mlab.com:51817/heroku_jnzbgb2c"
+    DATABASE = pymongo.MongoClient(URI).get_database()
 
     @staticmethod
     def insert(collection : str , data : Dict):
@@ -26,4 +25,5 @@ class Database(object):
     @staticmethod
     def remove(collection: str, query: Dict) -> Dict:
         return Database.DATABASE[collection].remove(query)
+
 
